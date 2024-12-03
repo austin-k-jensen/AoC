2db58277c2bd116ce6f76c678e0c35de098d76df
975601 <austin.jensen00@delta.com>
2024-12-03 12:10:49 -0500

    sql day 3 done

--- advent_of_sql/2024/day_03.sql
+++ advent_of_sql/2024/day_03.sql
@@ -6,8 +6,13 @@ with base_cte as(
       (xpath('//total_guests/text()', menu_data::xml))[1],
       (xpath('//total_present/text()', menu_data::xml))[1]
  	)::text::int AS guests,
-    unnest(xpath('//food_item_id/text()', menu_data::xml))
+    unnest(xpath('//food_item_id/text()', menu_data::xml))::text as food_item_id
   from christmas_menus
 )
-select * from base_cte
+select 
+  food_item_id,
+  count(*) as cnt 
+from base_cte
 where guests > 78
+group by food_item_id
+order by cnt desc
