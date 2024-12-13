f78682ac0a733f346597e9c1a8378c9327ebcc79
Austin Jensen <austinkj11@gmail.com>
2024-12-13 09:12:07 -0500

    Day 13 done

---
+++ 2024/day_13.py
@@ -0,0 +1,79 @@
+import re
+import numpy as np
+from math import isclose
+from aocd import get_data
+from utils import timing
+
+YEAR, DAY = 2024, 13
+puzzle = get_data(day=DAY, year=YEAR)
+
+TEST_1 = """
+Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279
+"""
+
+
+@timing
+def parse(data: str):
+    systems = []
+
+    for machine in data.strip().split("\n\n"):
+        numbers = [int(x) for x in re.findall(r"\d+", machine)]
+
+        systems.append(
+            [
+                np.array([[numbers[0], numbers[2]], [numbers[1], numbers[3]]]),
+                np.array([numbers[4], numbers[5]]),
+            ]
+        )
+    return systems
+
+
+@timing
+def part_1(systems):
+
+    tot = 0
+    for system in systems:
+        A, B = np.linalg.inv(system[0]).dot(system[1])
+        int_A = int(round(A))
+        int_B = int(round(B))
+
+        if isclose(A, int_A) and isclose(B, int_B):
+            tot += 3 * int_A + int_B
+    return tot
+
+
+@timing
+def part_2(systems):
+
+    tot = 0
+    for system in systems:
+        # print(system[1] + 10000000000000)
+
+        A, B = np.linalg.inv(system[0]).dot(system[1] + 10000000000000)
+        int_A = int(round(A))
+        int_B = int(round(B))
+
+        # print(A, B)
+        if isclose(A, int_A, rel_tol=1e-15) and isclose(B, int_B, rel_tol=1e-15):
+            # print(int_A, int_B)
+            tot += 3 * int_A + int_B
+    return tot
+
+
+systems = parse(puzzle)
+print("part 1: ", part_1(systems))
+print("part 2: ", part_2(systems))
