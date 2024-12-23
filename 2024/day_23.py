bcc7611450a86fa96f9938c9e9dcac6e125862a0
975601 <austin.jensen00@delta.com>
2024-12-23 10:40:18 -0500

    Combine parts using networkx

--- 2024/day_23.py
+++ 2024/day_23.py
@@ -41,21 +41,6 @@ td-yn
 """
 
 
-def parse(data: str):
-    graph = {}
-    for edge in data.strip().splitlines():
-        n1, n2 = edge.split("-")
-        if n1 in graph:
-            graph[n1].append(n2)
-        else:
-            graph[n1] = [n2]
-        if n2 in graph:
-            graph[n2].append(n1)
-        else:
-            graph[n2] = [n1]
-    return graph
-
-
 def create_graph(data: str):
     G = nx.Graph()
     for edge in data.strip().splitlines():
@@ -66,32 +51,21 @@ def create_graph(data: str):
 
 
 @timing
-def part_1(graph: dict):
-    groups = set()
-    for node1, nodes1 in graph.items():
-        # print(node1, nodes1)
-        for node2 in nodes1:
-            # print("\t", node2, graph[node2])
-            for node3 in graph[node2]:
-                # print("\t\t", node3, graph[node3])
-                if node1 in graph[node3] and node2 in graph[node3]:
-                    # print("\t\t\tSet found:", node1, node2, node3)
-                    if node1[0] == "t":
-                        groups.add(frozenset([node1, node2, node3]))
-    print(len(groups))
-
-
-def part_2(graph: nx.graph):
-
+def both(graph: nx.graph):
     best = 0
-    for clq in nx.clique.find_cliques(graph):
+    threes = []
+    for clq in nx.enumerate_all_cliques(graph):
+        if len(clq) == 3:
+            if clq[0][0] == "t" or clq[1][0] == "t" or clq[2][0] == "t":
+                threes.append(clq)
         if len(clq) > best:
             out = clq
             best = len(clq)
-    return ",".join(sorted(out))
+
+    return len(threes), ",".join(sorted(out))
 
 
-# graph = parse(puzzle)
-# part_1(graph)
 graph = create_graph(puzzle)
-print(part_2(graph))
+part_1, part_2 = both(graph)
+print("part 1: ", part_1)
+print("part 2: ", part_2)
