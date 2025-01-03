d176e4819e801543b8c9c6c8fd9ef27e7dfcb425
Austin Jensen <austinkj11@gmail.com>
2025-01-03 18:24:47 -0500

    2019 Day 8 Part 2

--- 2019/day_08.py
+++ 2019/day_08.py
@@ -4,12 +4,12 @@ YEAR, DAY = 2019, 8
 puzzle = get_data(day=DAY, year=YEAR)
 
 TEST_1 = "123456789012"
+TEST_2 = "0222112222120000"
 
 
-def part_1(data: str):
+def part_1(data: str, l: int, h: int):
 
-    # layer_len = 2 * 3
-    layer_len = 25 * 6
+    layer_len = l * h
     layer_cnt = len(data) // layer_len
     least_0 = 100
 
@@ -18,7 +18,27 @@ def part_1(data: str):
         if layer.count("0") < least_0:
             least_0 = layer.count("0")
             least_layer = layer
-    print(least_layer.count("1") * least_layer.count("2"))
+    return least_layer.count("1") * least_layer.count("2")
 
 
-part_1(puzzle)
+def part_2(data: str, l: int, h: int):
+    layer_len = l * h
+    layer_cnt = len(data) // layer_len
+
+    out = ""
+    for pos in range(layer_len):
+        for layer_ind in range(layer_cnt):
+            layer = data[layer_ind * layer_len : (layer_ind + 1) * layer_len]
+
+            if layer[pos] in "01":
+                out += layer[pos]
+                break
+
+    for layer_ind in range(h):
+        layer = out[layer_ind * l : (layer_ind + 1) * l]
+        print("\t", layer.replace("0", " ").replace("1", "#"))
+
+
+print("Part 1: ", part_1(puzzle, 25, 6))
+print("Part 2: ")
+part_2(puzzle, 25, 6)
