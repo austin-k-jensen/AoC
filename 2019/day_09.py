8c5f68acc50fbb2a7a1e9137b9bf93dbc0b2053d
Austin Jensen <austinkj11@gmail.com>
2025-01-03 23:14:26 -0500

    2019 Day 9

--- 2019/day_09.py
+++ 2019/day_09.py
@@ -25,40 +25,65 @@ def parse_instruction(instruction: int):
     return op_code, param_1_mode, param_2_mode, param_3_mode
 
 
-def get_param(memory: defaultdict, address: int, param_mode: int, inc: int):
-    if param_mode:
-        param = memory[address + inc]
-    else:
+def get_param(
+    memory: defaultdict, address: int, rel_base: int, param_mode: int, inc: int
+):
+    if param_mode == 0:
         param = memory[memory[address + inc]]
+    elif param_mode == 1:
+        param = memory[address + inc]
+    elif param_mode == 2:
+        param = memory[memory[address + inc] + rel_base]
+    else:
+        print("Error: Invalid Param Mode")
 
     return param
 
 
-def op_1(memory: defaultdict, address: int, param_1_mode: int, param_2_mode: int):
-    param_1 = get_param(memory, address, param_1_mode, 1)
-    param_2 = get_param(memory, address, param_2_mode, 2)
+def op_1(
+    memory: defaultdict,
+    address: int,
+    rel_base: int,
+    param_1_mode: int,
+    param_2_mode: int,
+    param_3_mode: int,
+):
+    param_1 = get_param(memory, address, rel_base, param_1_mode, 1)
+    param_2 = get_param(memory, address, rel_base, param_2_mode, 2)
+    inc = rel_base if param_3_mode == 2 else 0
 
-    memory[memory[address + 3]] = param_1 + param_2
+    memory[memory[address + 3] + inc] = param_1 + param_2
     address += 4
 
     return memory, address
 
 
-def op_2(memory: defaultdict, address: int, param_1_mode: int, param_2_mode: int):
-    param_1 = get_param(memory, address, param_1_mode, 1)
-    param_2 = get_param(memory, address, param_2_mode, 2)
+def op_2(
+    memory: defaultdict,
+    address: int,
+    rel_base: int,
+    param_1_mode: int,
+    param_2_mode: int,
+    param_3_mode: int,
+):
+    param_1 = get_param(memory, address, rel_base, param_1_mode, 1)
+    param_2 = get_param(memory, address, rel_base, param_2_mode, 2)
+    inc = rel_base if param_3_mode == 2 else 0
 
-    memory[memory[address + 3]] = param_1 * param_2
+    memory[memory[address + 3] + inc] = param_1 * param_2
     address += 4
 
     return memory, address
 
 
-def op_3(memory: defaultdict, address: int, input: list):
+def op_3(
+    memory: defaultdict, address: int, rel_base: int, param_1_mode: int, input: list
+):
+    inc = rel_base if param_1_mode == 2 else 0
 
     if len(input) > 0:
         input = input.pop(0)
-        memory[memory[address + 1]] = input
+        memory[memory[address + 1] + inc] = input
         address += 2
     else:
         print("Input to short")
@@ -67,16 +92,22 @@ def op_3(memory: defaultdict, address: int, input: list):
     return memory, address
 
 
-def op_4(memory: defaultdict, address: int, param_1_mode: int):
-    param_1 = get_param(memory, address, param_1_mode, 1)
+def op_4(memory: defaultdict, address: int, rel_base: int, param_1_mode: int):
+    param_1 = get_param(memory, address, rel_base, param_1_mode, 1)
     address += 2
 
     return address, param_1
 
 
-def op_5(memory: defaultdict, address: int, param_1_mode: int, param_2_mode: int):
-    param_1 = get_param(memory, address, param_1_mode, 1)
-    param_2 = get_param(memory, address, param_2_mode, 2)
+def op_5(
+    memory: defaultdict,
+    address: int,
+    rel_base: int,
+    param_1_mode: int,
+    param_2_mode: int,
+):
+    param_1 = get_param(memory, address, rel_base, param_1_mode, 1)
+    param_2 = get_param(memory, address, rel_base, param_2_mode, 2)
     if param_1 != 0:
         address = param_2
     else:
@@ -85,9 +116,15 @@ def op_5(memory: defaultdict, address: int, param_1_mode: int, param_2_mode: int
     return address
 
 
-def op_6(memory: defaultdict, address: int, param_1_mode: int, param_2_mode: int):
-    param_1 = get_param(memory, address, param_1_mode, 1)
-    param_2 = get_param(memory, address, param_2_mode, 2)
+def op_6(
+    memory: defaultdict,
+    address: int,
+    rel_base: int,
+    param_1_mode: int,
+    param_2_mode: int,
+):
+    param_1 = get_param(memory, address, rel_base, param_1_mode, 1)
+    param_2 = get_param(memory, address, rel_base, param_2_mode, 2)
     if param_1 == 0:
         address = param_2
     else:
@@ -96,34 +133,61 @@ def op_6(memory: defaultdict, address: int, param_1_mode: int, param_2_mode: int
     return address
 
 
-def op_7(memory: defaultdict, address: int, param_1_mode: int, param_2_mode: int):
-    param_1 = get_param(memory, address, param_1_mode, 1)
-    param_2 = get_param(memory, address, param_2_mode, 2)
+def op_7(
+    memory: defaultdict,
+    address: int,
+    rel_base: int,
+    param_1_mode: int,
+    param_2_mode: int,
+    param_3_mode: int,
+):
+    param_1 = get_param(memory, address, rel_base, param_1_mode, 1)
+    param_2 = get_param(memory, address, rel_base, param_2_mode, 2)
+    inc = rel_base if param_3_mode == 2 else 0
 
     if param_1 < param_2:
         out = 1
     else:
         out = 0
 
-    memory[memory[address + 3]] = out
+    memory[memory[address + 3] + inc] = out
     address += 4
 
     return memory, address
 
 
-def op_8(memory: defaultdict, address: int, param_1_mode: int, param_2_mode: int):
-    param_1 = get_param(memory, address, param_1_mode, 1)
-    param_2 = get_param(memory, address, param_2_mode, 2)
+def op_8(
+    memory: defaultdict,
+    address: int,
+    rel_base: int,
+    param_1_mode: int,
+    param_2_mode: int,
+    param_3_mode: int,
+):
+    param_1 = get_param(memory, address, rel_base, param_1_mode, 1)
+    param_2 = get_param(memory, address, rel_base, param_2_mode, 2)
+    inc = rel_base if param_3_mode == 2 else 0
 
     if param_1 == param_2:
         out = 1
     else:
         out = 0
 
-    memory[memory[address + 3]] = out
+    memory[memory[address + 3] + inc] = out
     address += 4
 
     return memory, address
+
+
+def op_9(memory: defaultdict, address: int, rel_base: int, param_1_mode: int):
+    param_1 = get_param(memory, address, rel_base, param_1_mode, 1)
+
+    rel_base += param_1
+    address += 2
+
+    # print("\tNew rel_base:", rel_base)
+
+    return memory, address, rel_base
 
 
 def run_program(
@@ -138,29 +202,74 @@ def run_program(
         if op_code == 99:
             break
         elif op_code == 1:
-            memory, address = op_1(memory, address, param_1_mode, param_2_mode)
+            memory, address = op_1(
+                memory, address, rel_base, param_1_mode, param_2_mode, param_3_mode
+            )
         elif op_code == 2:
-            memory, address = op_2(memory, address, param_1_mode, param_2_mode)
+            memory, address = op_2(
+                memory, address, rel_base, param_1_mode, param_2_mode, param_3_mode
+            )
         elif op_code == 3:
-            memory, address = op_3(memory, address, input)
+            memory, address = op_3(memory, address, rel_base, param_1_mode, input)
         elif op_code == 4:
-            address, output = op_4(memory, address, param_1_mode)
+            address, output = op_4(memory, address, rel_base, param_1_mode)
             # print("Output:", output)
             return output, memory, address, rel_base
         elif op_code == 5:
-            address = op_5(memory, address, param_1_mode, param_2_mode)
+            address = op_5(memory, address, rel_base, param_1_mode, param_2_mode)
         elif op_code == 6:
-            address = op_6(memory, address, param_1_mode, param_2_mode)
+            address = op_6(memory, address, rel_base, param_1_mode, param_2_mode)
         elif op_code == 7:
-            memory, address = op_7(memory, address, param_1_mode, param_2_mode)
+            memory, address = op_7(
+                memory, address, rel_base, param_1_mode, param_2_mode, param_3_mode
+            )
         elif op_code == 8:
-            memory, address = op_8(memory, address, param_1_mode, param_2_mode)
+            memory, address = op_8(
+                memory, address, rel_base, param_1_mode, param_2_mode, param_3_mode
+            )
+        elif op_code == 9:
+            memory, address, rel_base = op_9(memory, address, rel_base, param_1_mode)
         else:
             print("error")
+            return None, memory, address, rel_base
 
         # print(memory)
 
     return None, memory, address, rel_base
 
 
+def part_1(program: defaultdict):
+
+    memory = program.copy()
+    address = 0
+    rel_base = 0
+    input = [1]
+
+    while True:
+        output, memory, address, rel_base = run_program(
+            memory, address, rel_base, input
+        )
+        if output == None:
+            break
+        print("Output:", output)
+
+
+def part_2(program: defaultdict):
+
+    memory = program.copy()
+    address = 0
+    rel_base = 0
+    input = [2]
+
+    while True:
+        output, memory, address, rel_base = run_program(
+            memory, address, rel_base, input
+        )
+        if output == None:
+            break
+        print("Output:", output)
+
+
 program = parse(puzzle)
+part_1(program)
+part_2(program)
